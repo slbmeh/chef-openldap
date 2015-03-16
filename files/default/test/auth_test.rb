e8e266d50a46be8fafdcfa432da9355ef0c901dd
Steve Buzonas <steve@fancyguy.com>
2015-03-16 07:53:42 -0400

    update anonymous bind test to utilize configured server uri

--- files/default/test/auth_test.rb
+++ files/default/test/auth_test.rb
@@ -1,7 +1,6 @@
 describe_recipe 'openldap::auth' do
   it 'can bind anonymously' do
-    # TODO: test anonomously binding to all configured protocols
-    output = `/usr/bin/ldapwhoami -x -H ldapi:/// 2>&1`
+    output = `/usr/bin/ldapwhoami -x -H #{node['openldap']['server_uri']} 2>&1`
     assert_match /anonymous/, output
     # TODO: consider `require "English"' to change this back to the more readable $CHILD_STATUS
     assert_equal 0, $?.exitstatus
