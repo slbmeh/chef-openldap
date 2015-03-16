fe575694af4f4ed1957d049f9605d9f54064389e
Steve Buzonas <steve@fancyguy.com>
2015-03-16 06:56:32 -0400

    all tests passing for server-ubuntu-1204 as baseline

--- files/default/test/auth_test.rb
+++ files/default/test/auth_test.rb
@@ -1,7 +1,9 @@
 describe_recipe 'openldap::auth' do
   it 'can bind anonymously' do
-    output = `/usr/bin/ldapwhoami -x 2>&1`
+    # TODO: test anonomously binding to all configured protocols
+    output = `/usr/bin/ldapwhoami -x -H ldapi:/// 2>&1`
     assert_match /anonymous/, output
-    assert_equal 0, $CHILD_STATUS.exitstatus
+    # TODO: consider `require "English"' to change this back to the more readable $CHILD_STATUS
+    assert_equal 0, $?.exitstatus
   end
 end
